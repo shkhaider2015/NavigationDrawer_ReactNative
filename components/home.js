162b9739eb28f950fe8b2bee48b69677a48e9a1d
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-11-26 16:51:14 +0500

    sjdhfh

---
+++ components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from "react-native";
+
+
+class home extends React.Component {
+
+    
+    render() {
+        return (
+            <view>
+                
+            </view>
+        );
+    }
+}
+
+export default home;
---
+++ components/navigationDrawer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from "react-native";
+
+class navigationDrawer extends React.Component {
+
+    
+    render() {
+        return (
+            <view>
+                
+            </view>
+        );
+    }
+}
+
+export default navigationDrawer;
